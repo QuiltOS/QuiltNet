50f16651acc50b75ff2360d43889323f4a35dc21
anrosent <Anson_Rosenthal@brown.edu>
2014-11-08 14:34:10 -0500

    added TcpPacket getters with handrolled get_multibyte thing which works locally (little endian), going to switch to BufReader.read_be_u** to be sure this works

--- transport/tcp/src/packet.rs
+++ transport/tcp/src/packet.rs
@@ -77,8 +77,7 @@ impl TcpPacket {
     self.ip.borrow().get_source()
   }
   pub fn get_src_port(&self) -> u16 {
-    //TODO:
-    0
+    Int::from_be(get_multibyte(self.tcp_hdr(), 0, 2) as u16)
   }
   pub fn set_src_port(&mut self, port: u16) {
     //TODO:
@@ -87,8 +86,7 @@ impl TcpPacket {
     self.ip.borrow().get_destination()
   }
   pub fn get_dst_port(&self) -> u16 {
-    //TODO:
-    0
+    Int::from_be(get_multibyte(self.tcp_hdr(), 2, 2) as u16)
   }
   pub fn set_dst_port(&mut self, port: u16) {
     //TODO
@@ -96,46 +94,49 @@ impl TcpPacket {
 
   // Control Flags
   pub fn is_ack(&self) -> bool {
-    //TODO:
-    false
+    self.tcp_hdr()[13] & 16 != 0
   }
   pub fn set_ack(&mut self) {
     //TODO
   }
+  // Not sure if this is used
+  pub fn is_rst(&self) -> bool {
+    self.tcp_hdr()[13] & 4 != 0
+  }
+  pub fn set_rst(&mut self) {
+    //TODO
+  }
   pub fn is_syn(&self) -> bool {
-    //TODO:
-    false
+    self.tcp_hdr()[13] & 2 != 0
   }
   pub fn set_syn(&mut self) {
     //TODO
   }
   pub fn is_fin(&self) -> bool {
-    //TODO:
-    false
+    self.tcp_hdr()[13] & 1 != 0
   }
   pub fn set_fin(&mut self) {
     //TODO
   }
 
-  // Not sure if this is used
-  pub fn is_rst(&self) -> bool {
-    //TODO:
-    false
+
+  // Recv Window size
+  pub fn get_window_size(&self) -> u16 {
+    Int::from_be(get_multibyte(self.tcp_hdr(), 14, 2)) as u16
   }
-  pub fn set_rst(&mut self) {
+  pub fn set_window_size(&mut self) {
     //TODO
   }
 
-  // Other TCP data
-  pub fn get_hdr_size(&self) -> u8 { // really u8
-    //TODO:
-    0
+  // AKA data offset: 4 bytes
+  pub fn get_hdr_size(&self) -> u8 {
+    (self.tcp_hdr()[12] >> 4) as u8
   }
 
   // Sequence Number Ops
   pub fn get_seq_num(&self) -> u32 {
-    //TODO:
-    0
+    // assert!(self.is_seq())
+    Int::from_be(get_multibyte(self.tcp_hdr(), 4, 4)) as u32
   }
   pub fn set_seq_num(&mut self, seq_num: u16) {
     //TODO:
@@ -143,9 +144,8 @@ impl TcpPacket {
 
   // Acknowledgement Number Ops
   pub fn get_ack_num(&self) -> u32 {
-    //TODO:
     assert!(self.is_ack());
-    0
+    Int::from_be(get_multibyte(self.tcp_hdr(), 8, 4)) as u32
   }
   pub fn set_ack_num(&mut self, ack_num: u16) {
     //TODO:
@@ -153,8 +153,7 @@ impl TcpPacket {
 
   // Checksum Ops
   pub fn get_checksum(&self) -> u16 {
-    //TODO:
-    0
+    Int::from_be(get_multibyte(self.tcp_hdr(), 16, 2)) as u16
   }
   pub fn compute_checksum(&self) -> u16 {
     //TODO:
@@ -176,6 +175,48 @@ impl TcpPacket {
   /// Returns TCP payload as mut slice
   pub fn get_mut_payload(&mut self) -> &mut[u8] {
     self.get_tcp_mut()[mut TCP_HDR_LEN..]
+  }
+
+}
+
+// Reads int in BE order
+// TODO: make sure endianness is alright: I'm calling Int::from_be on result
+// to read multibyte fields from packet headers
+fn get_multibyte(buf: &[u8], start_ix: uint, len: uint) -> int {
+  let mut res = 0i;
+
+  // Iterate over bytes in reverse order, increasing significance
+  for (ix, v) in  buf[start_ix..start_ix + len].iter().rev().enumerate() {
+     res |= ((*v) as int << (8 * ix));
+  }
+  res
+}
+
+#[cfg(test)]
+mod test {
+  use super::get_multibyte;
+
+  #[test]
+  fn byte(){
+    let b = [1u8]; // 0x1 == 1
+    assert!(get_multibyte(b, 0, 1) == 1i);
+  }
+  
+  #[test]
+  fn short(){
+    let b = [1u8, 255u8]; // 0x1_11111111 == 511
+    assert!(get_multibyte(b, 0, 1) == 1i);
+    assert!(get_multibyte(b, 0, 2) == 511i);
+  }
+
+  #[test]
+  fn u32(){
+    let b = [10u8, 112u8, 1u8, 123u8]; // 0x00001010_01110000_00000001_01111011 == 511
+    assert!(get_multibyte(b, 0, 1) == 10i);
+    assert!(get_multibyte(b, 0, 2) == 2672i);
+    assert!(get_multibyte(b, 0, 3) == 684033i);
+    assert!(get_multibyte(b, 0, 4) == 175112571);
+    assert!(false);
   }
 
 }
