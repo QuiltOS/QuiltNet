4088471cf389dd9a360a8fc4cdf11df9dade7aa4
John Ericson <Ericson2314@Yahoo.com>
2014-11-25 19:09:08 -0500

    Fix warnings in tcp::packet_buf (including children)

--- transport/tcp/src/connection/packet_buf/vec_queue.rs
+++ transport/tcp/src/connection/packet_buf/vec_queue.rs
@@ -91,14 +91,14 @@ impl super::PacketBuf for PacketBuf
 
   fn add_vec(&mut self, seq_num: u32, vec: Vec<u8>, start_off: uint) -> u32
   {
-    let U32_MAX: u32 = Int::max_value();
+    let u32_max: u32 = Int::max_value();
 
     let node = Tagged {
       seq:    seq_num,
       offset: start_off,
       buf:    vec,
     };
-    assert!(node.len() < U32_MAX as uint);
+    assert!(node.len() < u32_max as uint);
 
 
     let ind = self.find_index(&node);
@@ -156,10 +156,10 @@ fn verify_adjacent(sm: &Tagged, lg: &Tagged)
 /// Stops when there is a discontinuity
 fn no_gap(sm: &Tagged, lg: &Tagged) -> bool
 {
-  let U32_MAX: u32 = Int::max_value();
+  let u32_max: u32 = Int::max_value();
 
   let normal = sm.head() >= lg.tail();
-  let wrap   = sm.head() >= (lg.tail() + U32_MAX as u64);
+  let wrap   = sm.head() >= (lg.tail() + u32_max as u64);
   normal || wrap
 }
 
@@ -171,10 +171,10 @@ fn no_gap(sm: &Tagged, lg: &Tagged) -> bool
 /// Returns false when `lg` is wholly redundant. Expects no gap.
 fn no_subsumption(sm: &Tagged, lg: &Tagged) -> bool
 {
-  let U32_MAX: u32 = Int::max_value();
+  let u32_max: u32 = Int::max_value();
 
   let normal = sm.head() <= lg.head();
-  let wrap   = sm.head() <= (lg.head() + U32_MAX as u64);
+  let wrap   = sm.head() <= (lg.head() + u32_max as u64);
   ! (normal || wrap)
 }
 
@@ -223,8 +223,8 @@ fn fine_iter<'a, I>(iter: I) -> Fine<'a, I>
     let to = if cur.seq < next.seq {
       (next.seq - cur.seq) as uint
     } else {
-      let U32_MAX: u32 = Int::max_value();
-      (U32_MAX as u64 + next.seq as u64 - cur.seq as u64) as uint
+      let u32_max: u32 = Int::max_value();
+      (u32_max as u64 + next.seq as u64 - cur.seq as u64) as uint
     };
 
     cur.as_slice()[..to].iter()
